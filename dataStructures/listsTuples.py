465b998df5573098f152282133d2399b452e5e1b
laromero226 <alejandraromerom1402@gmail.com>
2025-09-10 15:42:30 -0500

    Update listsTuples.py
    me falta terminar varias lineas

--- dataStructures/listsTuples.py
+++ dataStructures/listsTuples.py
@@ -1,46 +1,46 @@
 #################LISTAS####################
 ###########################################
-my_lista = ['Rojo', 'Azul', 'Amarillo', 'Naranja', 'Violeta', 'Verde']
+my_lista = ['Rojo', 'Azul', 'Amarillo', 'Naranja', 'Violeta', 'Verde'] #aqui estan todos los elementos de la lsita determinados#
 #input()
 print(my_lista)
-print(type(my_lista))
-print(my_lista[2])
+print(type(my_lista)) 
+print(my_lista[2]) #aqui me va a mostrar hasta que numero de la lista tenemos osea va hasta el elemento 2#
 
-print("my_lista size: ", len(my_lista))
-print(my_lista[0:2])
-print(my_lista[:2])
+print("my_lista size: ", len(my_lista)) #nos muestra el tamaño de estas#
+print(my_lista[0:2])#aqui nos diga que se va mostrar desde el elemento 0 a el elemento 2 no lo incluye#
+print(my_lista[:2])# nos muestra que esta lsita inicia desde cero hasta elemento numero 2 pero sin incluirlo #
 
 my_lista.append('Blanco')      #Agrega elemento al final de la lista
-print(my_lista)
+print(my_lista) #muestra la lista #
 
-my_lista.insert(3, 'Negro')
-print(my_lista)
+my_lista.insert(3, 'Negro') # se pone este elemento en  el numero de casilla que se indica ahi#
+print(my_lista) # muestra la lista# 
 
 
 my_lista.extend(['Marron', 'Gris'])   #Concatena a otra lista
-print(my_lista)
+print(my_lista)#mostrar la lista#
 
-print(my_lista.index('Azul'))
+print(my_lista.index('Azul')) # busca la posicion donde esta azul#
 
 #my_lista.remove('Magenta')
-my_lista.remove('Marron')
+my_lista.remove('Marron') # busca este elemento en la lista sino esta lo elimina#
 print(my_lista)
 
-my_lista.insert(8, 'Marron')
+my_lista.insert(8, 'Marron') # agrega el color marron en la posicion numero 8 #
 print(my_lista)
 
-print(my_lista.pop())
-size = len(my_lista)
+print(my_lista.pop()) # elimina y devuelve al ultimo elemento de la lista#
+size = len(my_lista) #cantidad de elementos en una lista#
 print("size = ", size)
 #print(my_lista.pop(size))
 
-my_lista_3 = my_lista*3
-print("my_lista_3: ", my_lista_3)
+my_lista_3 = my_lista*3 #muestra los elemento 3 veces # 
+print("my_lista_3: ", my_lista_3) # mostrar la lista#
 
-print("Sort:")
+print("Sort:") # ordenar la lista # 
 print()
 my_listaSort = my_lista.sort()
-print(my_listaSort)
+print(my_listaSort) # mostrar la lisra
 
 my_NumList = [10, 9, 8, 7, 6 , 5 , 4, 3, 2, 1]
 print("Ordering my_NumList: ")
